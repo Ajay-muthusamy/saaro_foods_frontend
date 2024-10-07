8e70a79645a00aef04ba1a3a8e66f4e98d53fe8e
DineshKarthikRajanD <dineshkarthikrajand.22cse@kongu.edu>
2024-10-07 14:55:23 +0530

    navbar 1

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <main></main>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import { IoMdCall } from "react-icons/io";
 
 const Navbar = () => {
   return (
     <div>
-      <nav>
-        <ul>
-          <li>
+      <nav className="bg-[#C11900] text-[#FFDB00] font-thin">
+        <ul className="flex justify-between text-xl py-4">
+          <li className="ml-20">
             <h1>Logo</h1>
           </li>
-          <li>
-            <h1></h1>
+          <li className="flex gap-3 mr-20">
+            <IoMdCall className="mt-1 h-7 w-7" />
+            <h1>+91-98765 43210</h1>
           </li>
         </ul>
       </nav>
