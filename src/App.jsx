9549c13c65e1a7a5e81151858493e828b29a6e75
Ajay-muthusamy <ajaym.22cse@kongu.edu>
2024-10-08 09:09:35 +0530

    navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeLayout from "./components/Home/HomeLayout";
+import Product from "./Product/Product";
 
 const App = () => {
   return (
     <div>
-      <HomeLayout />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeLayout />}/>
+          <Route path="/shop-now" element={<Product />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import Lottie from "lottie-react";
+import { useNavigate } from "react-router-dom";
 import animationData from "../../animefiles/Animation - 1728323934657.json";
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <div>
-      <main className="flex-col md:flex-row flex items-center justify-center space-x-9 " id="home">
+      <main
+        className="flex-col md:flex-row flex items-center justify-center space-x-9 "
+        id="home"
+      >
         <div className="font-ubuntu ">
-          <h2 className="text-[6vh] leading-[9vh] mt-3 x w-[50vh] text-center md:text-left md:text-[8vh] md:w-[70vh] md:leading-[10vh]">Indulge in the Sweetest <span className="text-rose-400">Delights</span></h2>
-          <p className="text-center md:text-left ps-2 text-sm text-rose-400">Freshly made sweets delivered to your doorstep!</p>
+          <h2 className="text-[6vh] leading-[9vh] mt-3 x w-[50vh] text-center md:text-left md:text-[8vh] md:w-[70vh] md:leading-[10vh]">
+            Indulge in the Sweetest{" "}
+            <span className="text-rose-400">Delights</span>
+          </h2>
+          <p className="text-center md:text-left ps-2 text-sm text-rose-400">
+            Freshly made sweets delivered to your doorstep!
+          </p>
+          <button className="shop-now-btn text-white py-3 px-8 rounded-full text-lg font-semibold transform transition duration-300 ease-in-out hover:scale-110 hover:shadow-lg focus:outline-none animated-gradient mt-5" onClick={()=>navigate('/shop-now')}>
+            Shop Now
+          </button>
         </div>
         <div>
           <Lottie
