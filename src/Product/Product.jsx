0d5f5e9da5437e2db3b99de84adbe6fbd79ebdaf
Ajay-muthusamy <ajaym.22cse@kongu.edu>
2024-10-08 09:45:18 +0530

    cart

--- src/Product/Product.jsx
+++ src/Product/Product.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import dryFruites from "../json/dryfruit.json";
 
 const Product = () => {
   return (
-    <div>Product</div>
-  )
-}
+    <section>
+      <div>
+        {/* {leave it} */}
+      </div>
+      <div>
+        <div>
+          {dryFruites.map((data) => (
+            <div>
+              <img src={data.img} alt="" />
+              <h1>{data.title}</h1>
+              <h1>{data.price}</h1>
+              {!data.stock ? <span className="text-red-500">Out of stock</span> : <span></span>}
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Product
+export default Product;
